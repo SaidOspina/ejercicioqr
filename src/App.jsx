e91bb24de3d0986da1dcf24452c8cedfc7c30cfc
SaidOspina <deislersaid1418@gmail.com>
2025-08-04 12:02:28 -0500

    ft:Update project for activity

--- src/App.jsx
+++ src/App.jsx
@@ -1,34 +1,35 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import React from 'react'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
+  <>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
+        <div className="qr-container">
+          <div className="qr-code">
+            <img
+              src="https://res.cloudinary.com/dqaxadodm/image/upload/v1754161169/qr-code-white-icon-vector-removebg-preview_dn3dz0.png"
+              alt="QR Code"
+              className="qr-image"
+            />
+          </div>
+        </div>
+                
+        <div className="content">
+          <h2 className="title">
+            Improve your front-end<br />
+            skills by building projects
+          </h2>
+                    
+          <p className="description">
+            Scan the QR code to visit Frontend<br />
+            Mentor and take your coding skills to<br />
+            the next level
+          </p>
+        </div>
       </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+  </>
+    
   )
 }
 
